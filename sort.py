8e2740db097806ecd693e4bdbf11002ea78627c4
Aditya Kumar <93466706+adityakumar-in@users.noreply.github.com>
2022-10-01 13:55:27 +0000

    Improved grametical mistakes

--- sort.py
+++ sort.py
@@ -15,7 +15,7 @@
 """
 
 def sort_blocks():
-    # First, we load the current README into memory
+    # First, We load the current README into memory
     with open('README.md', 'r') as read_me_file:
         read_me = read_me_file.read()
 
