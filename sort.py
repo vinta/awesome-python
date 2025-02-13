4c9b0e50cd4048e1f63ade8dac4b7177b893df5b
MayureshMore <m_more1@u.pacific.edu>
2025-02-13 11:58:42 -0800

    [Edited] Add docstring to improve documentation

--- sort.py
+++ sort.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 """
     The approach taken is explained below. I decided to do it simply.
     Initially I was considering parsing the data into some sort of
@@ -15,11 +12,9 @@
 """
 
 def sort_blocks():
-    # First, we load the current README into memory
+    '''"""Auto-generated docstring for function 'sort_blocks'."""'''
     with open('README.md', 'r') as read_me_file:
         read_me = read_me_file.read()
-
-    # Separating the 'table of contents' from the contents (blocks)
     table_of_contents = ''.join(read_me.split('- - -')[0])
     blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
     for i in range(len(blocks)):
@@ -27,36 +22,25 @@ def sort_blocks():
             blocks[i] = blocks[i] + '\n'
         else:
             blocks[i] = '# ' + blocks[i] + '\n'
-
-    # Sorting the libraries
     inner_blocks = sorted(blocks[0].split('##'))
     for i in range(1, len(inner_blocks)):
         if inner_blocks[i][0] != '#':
             inner_blocks[i] = '##' + inner_blocks[i]
     inner_blocks = ''.join(inner_blocks)
-
-    # Replacing the non-sorted libraries by the sorted ones and gathering all at the final_README file
     blocks[0] = inner_blocks
     final_README = table_of_contents + '- - -' + ''.join(blocks)
-
     with open('README.md', 'w+') as sorted_file:
         sorted_file.write(final_README)
 
 def main():
-    # First, we load the current README into memory as an array of lines
+    '''"""Auto-generated docstring for function 'main'."""'''
     with open('README.md', 'r') as read_me_file:
         read_me = read_me_file.readlines()
-
-    # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
     blocks = []
     last_indent = None
     for line in read_me:
         s_line = line.lstrip()
         indent = len(line) - len(s_line)
-
         if any([s_line.startswith(s) for s in ['* [', '- [']]):
             if indent == last_indent:
                 blocks[-1].append(line)
@@ -66,20 +50,9 @@ def main():
         else:
             blocks.append([line])
             last_indent = None
-
     with open('README.md', 'w+') as sorted_file:
-        # Then all of the blocks are sorted individually
-        blocks = [
-            ''.join(sorted(block, key=str.lower)) for block in blocks
-        ]
-        # And the result is written back to README.md
+        blocks = [''.join(sorted(block, key=str.lower)) for block in blocks]
         sorted_file.write(''.join(blocks))
-
-    # Then we call the sorting method
     sort_blocks()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-# Automated edit: [Edited] Add docstring to improve documentation
